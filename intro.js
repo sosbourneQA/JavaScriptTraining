998300cf28833c753ea9c6932f64f50d16a7b6ae
luke <lukesosbourne@gmail.com>
2020-04-21 12:48:54 +0100

    first commit

---
+++ intro.js
@@ -0,0 +1,20 @@
+// document.write("howdy")
+
+
+let c = 10;
+console.log(c);
+
+let incX = 10;
+console.log(incX++);
+
+let age = 55;
+let message = "you are";
+
+if (50 < age) {
+    console.log(`${message}` + " old")
+} else {
+    console.log(`${message}` + " young")
+}
+
+
+console.log(age > 50 ? `${message}` + " old" : `${message}` + " young");
